4feb7d873a087bf5abc076fd318d9b5f950e6d58
iowen8 <iowen@Owens-iMac.fios-router.home>
2014-12-26 18:25:34 -0500

    iOS fix

---
+++ TriCare/TriCare.iOS/SqLite_iOS.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using TriCare.iOS;
+using TriCare.Data;
+using Xamarin.Forms;
+using System.IO;
+
+[assembly: Dependency (typeof (SqLite_iOS))]
+namespace TriCare.iOS
+{
+	public class SqLite_iOS : ISqLite
+	{
+		public SqLite_iOS ()
+		{
+		}
+		public SQLite.Net.SQLiteConnection GetConnection ()
+		{
+			var sqliteFilename = "TriCareSQLite.db3";
+			string documentsPath = Environment.GetFolderPath (Environment.SpecialFolder.Personal); // Documents folder
+			string libraryPath = Path.Combine (documentsPath, "..", "Library"); // Library folder
+			var path = Path.Combine(libraryPath, sqliteFilename);
+			// Create the connection
+			var plat = new SQLite.Net.Platform.XamarinIOS.SQLitePlatformIOS();
+			var conn = new SQLite.Net.SQLiteConnection(plat, path);
+			// Return the database connection
+			return conn;
+		}
+	}
+}
+
