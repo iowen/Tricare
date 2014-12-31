f30e25068064b34fe652eef206a0541c74737782
iowen <owatson@1of1inc.com>
2014-12-31 00:54:10 -0500

    fix

--- TriCare/TriCare.Android/Resources/Resource.Designer.cs
+++ TriCare/TriCare.Android/Resources/Resource.Designer.cs
@@ -28,6 +28,41 @@ namespace TriCare.Droid
 		{
 			global::SQLite.Net.Platform.XamarinAndroid.Resource.String.ApplicationName = global::TriCare.Droid.Resource.String.ApplicationName;
 			global::SQLite.Net.Platform.XamarinAndroid.Resource.String.Hello = global::TriCare.Droid.Resource.String.Hello;
+			global::Xamarin.Forms.Labs.Droid.Resource.Color.calendar_active_month_bg = global::TriCare.Droid.Resource.Color.calendar_active_month_bg;
+			global::Xamarin.Forms.Labs.Droid.Resource.Color.calendar_bg = global::TriCare.Droid.Resource.Color.calendar_bg;
+			global::Xamarin.Forms.Labs.Droid.Resource.Color.calendar_divider = global::TriCare.Droid.Resource.Color.calendar_divider;
+			global::Xamarin.Forms.Labs.Droid.Resource.Color.calendar_highlighted_day_bg = global::TriCare.Droid.Resource.Color.calendar_highlighted_day_bg;
+			global::Xamarin.Forms.Labs.Droid.Resource.Color.calendar_inactive_month_bg = global::TriCare.Droid.Resource.Color.calendar_inactive_month_bg;
+			global::Xamarin.Forms.Labs.Droid.Resource.Color.calendar_selected_day_bg = global::TriCare.Droid.Resource.Color.calendar_selected_day_bg;
+			global::Xamarin.Forms.Labs.Droid.Resource.Color.calendar_selected_range_bg = global::TriCare.Droid.Resource.Color.calendar_selected_range_bg;
+			global::Xamarin.Forms.Labs.Droid.Resource.Color.calendar_text_active = global::TriCare.Droid.Resource.Color.calendar_text_active;
+			global::Xamarin.Forms.Labs.Droid.Resource.Color.calendar_text_inactive = global::TriCare.Droid.Resource.Color.calendar_text_inactive;
+			global::Xamarin.Forms.Labs.Droid.Resource.Color.calendar_text_selected = global::TriCare.Droid.Resource.Color.calendar_text_selected;
+			global::Xamarin.Forms.Labs.Droid.Resource.Color.calendar_text_selector = global::TriCare.Droid.Resource.Color.calendar_text_selector;
+			global::Xamarin.Forms.Labs.Droid.Resource.Color.calendar_text_unselectable = global::TriCare.Droid.Resource.Color.calendar_text_unselectable;
+			global::Xamarin.Forms.Labs.Droid.Resource.Dimension.calendar_day_headers_paddingbottom = global::TriCare.Droid.Resource.Dimension.calendar_day_headers_paddingbottom;
+			global::Xamarin.Forms.Labs.Droid.Resource.Dimension.calendar_month_title_bottommargin = global::TriCare.Droid.Resource.Dimension.calendar_month_title_bottommargin;
+			global::Xamarin.Forms.Labs.Droid.Resource.Dimension.calendar_month_topmargin = global::TriCare.Droid.Resource.Dimension.calendar_month_topmargin;
+			global::Xamarin.Forms.Labs.Droid.Resource.Dimension.calendar_text_medium = global::TriCare.Droid.Resource.Dimension.calendar_text_medium;
+			global::Xamarin.Forms.Labs.Droid.Resource.Dimension.calendar_text_small = global::TriCare.Droid.Resource.Dimension.calendar_text_small;
+			global::Xamarin.Forms.Labs.Droid.Resource.Drawable.ad16 = global::TriCare.Droid.Resource.Drawable.ad16;
+			global::Xamarin.Forms.Labs.Droid.Resource.Drawable.calendar_bg_selector = global::TriCare.Droid.Resource.Drawable.calendar_bg_selector;
+			global::Xamarin.Forms.Labs.Droid.Resource.Id.calendar_grid = global::TriCare.Droid.Resource.Id.calendar_grid;
+			global::Xamarin.Forms.Labs.Droid.Resource.Id.calendar_view = global::TriCare.Droid.Resource.Id.calendar_view;
+			global::Xamarin.Forms.Labs.Droid.Resource.Id.title = global::TriCare.Droid.Resource.Id.title;
+			global::Xamarin.Forms.Labs.Droid.Resource.Layout.calendar_picker = global::TriCare.Droid.Resource.Layout.calendar_picker;
+			global::Xamarin.Forms.Labs.Droid.Resource.Layout.dialog = global::TriCare.Droid.Resource.Layout.dialog;
+			global::Xamarin.Forms.Labs.Droid.Resource.Layout.month = global::TriCare.Droid.Resource.Layout.month;
+			global::Xamarin.Forms.Labs.Droid.Resource.Layout.week = global::TriCare.Droid.Resource.Layout.week;
+			global::Xamarin.Forms.Labs.Droid.Resource.String.day_name_format = global::TriCare.Droid.Resource.String.day_name_format;
+			global::Xamarin.Forms.Labs.Droid.Resource.String.full_date_format = global::TriCare.Droid.Resource.String.full_date_format;
+			global::Xamarin.Forms.Labs.Droid.Resource.String.invalid_date = global::TriCare.Droid.Resource.String.invalid_date;
+			global::Xamarin.Forms.Labs.Droid.Resource.String.library_name = global::TriCare.Droid.Resource.String.library_name;
+			global::Xamarin.Forms.Labs.Droid.Resource.String.month_name_format = global::TriCare.Droid.Resource.String.month_name_format;
+			global::Xamarin.Forms.Labs.Droid.Resource.Style.CalendarCell = global::TriCare.Droid.Resource.Style.CalendarCell;
+			global::Xamarin.Forms.Labs.Droid.Resource.Style.CalendarCell_CalendarDate = global::TriCare.Droid.Resource.Style.CalendarCell_CalendarDate;
+			global::Xamarin.Forms.Labs.Droid.Resource.Style.CalendarCell_DayHeader = global::TriCare.Droid.Resource.Style.CalendarCell_DayHeader;
+			global::Xamarin.Forms.Labs.Droid.Resource.Style.CalendarTitle = global::TriCare.Droid.Resource.Style.CalendarTitle;
 		}
 		
 		public partial class Attribute
@@ -43,14 +78,97 @@ namespace TriCare.Droid
 			}
 		}
 		
+		public partial class Color
+		{
+			
+			// aapt resource value: 0x7f040000
+			public const int calendar_active_month_bg = 2130968576;
+			
+			// aapt resource value: 0x7f040001
+			public const int calendar_bg = 2130968577;
+			
+			// aapt resource value: 0x7f040002
+			public const int calendar_divider = 2130968578;
+			
+			// aapt resource value: 0x7f040005
+			public const int calendar_highlighted_day_bg = 2130968581;
+			
+			// aapt resource value: 0x7f040003
+			public const int calendar_inactive_month_bg = 2130968579;
+			
+			// aapt resource value: 0x7f040004
+			public const int calendar_selected_day_bg = 2130968580;
+			
+			// aapt resource value: 0x7f040006
+			public const int calendar_selected_range_bg = 2130968582;
+			
+			// aapt resource value: 0x7f040008
+			public const int calendar_text_active = 2130968584;
+			
+			// aapt resource value: 0x7f040007
+			public const int calendar_text_inactive = 2130968583;
+			
+			// aapt resource value: 0x7f040009
+			public const int calendar_text_selected = 2130968585;
+			
+			// aapt resource value: 0x7f04000b
+			public const int calendar_text_selector = 2130968587;
+			
+			// aapt resource value: 0x7f04000a
+			public const int calendar_text_unselectable = 2130968586;
+			
+			static Color()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Color()
+			{
+			}
+		}
+		
+		public partial class Dimension
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int calendar_day_headers_paddingbottom = 2131034112;
+			
+			// aapt resource value: 0x7f050002
+			public const int calendar_month_title_bottommargin = 2131034114;
+			
+			// aapt resource value: 0x7f050001
+			public const int calendar_month_topmargin = 2131034113;
+			
+			// aapt resource value: 0x7f050003
+			public const int calendar_text_medium = 2131034115;
+			
+			// aapt resource value: 0x7f050004
+			public const int calendar_text_small = 2131034116;
+			
+			static Dimension()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Dimension()
+			{
+			}
+		}
+		
 		public partial class Drawable
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int ad16 = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int tricareBG = 2130837505;
+			public const int calendar_bg_selector = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int Icon = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int tricareBG = 2130837507;
 			
 			static Drawable()
 			{
@@ -65,17 +183,26 @@ namespace TriCare.Droid
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050002
-			public const int btnCancel = 2131034114;
+			// aapt resource value: 0x7f080005
+			public const int btnCancel = 2131230725;
 			
-			// aapt resource value: 0x7f050003
-			public const int btnSave = 2131034115;
+			// aapt resource value: 0x7f080006
+			public const int btnSave = 2131230726;
 			
-			// aapt resource value: 0x7f050000
-			public const int rootView = 2131034112;
+			// aapt resource value: 0x7f080002
+			public const int calendar_grid = 2131230722;
 			
-			// aapt resource value: 0x7f050001
-			public const int signatureView = 2131034113;
+			// aapt resource value: 0x7f080000
+			public const int calendar_view = 2131230720;
+			
+			// aapt resource value: 0x7f080003
+			public const int rootView = 2131230723;
+			
+			// aapt resource value: 0x7f080004
+			public const int signatureView = 2131230724;
+			
+			// aapt resource value: 0x7f080001
+			public const int title = 2131230721;
 			
 			static Id()
 			{
@@ -91,7 +218,19 @@ namespace TriCare.Droid
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int SignaturePad = 2130903040;
+			public const int calendar_picker = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int dialog = 2130903041;
+			
+			// aapt resource value: 0x7f030002
+			public const int month = 2130903042;
+			
+			// aapt resource value: 0x7f030003
+			public const int SignaturePad = 2130903043;
+			
+			// aapt resource value: 0x7f030004
+			public const int week = 2130903044;
 			
 			static Layout()
 			{
@@ -106,11 +245,26 @@ namespace TriCare.Droid
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int ApplicationName = 2130968577;
+			// aapt resource value: 0x7f060006
+			public const int ApplicationName = 2131099654;
 			
-			// aapt resource value: 0x7f040000
-			public const int Hello = 2130968576;
+			// aapt resource value: 0x7f060005
+			public const int Hello = 2131099653;
+			
+			// aapt resource value: 0x7f060001
+			public const int day_name_format = 2131099649;
+			
+			// aapt resource value: 0x7f060004
+			public const int full_date_format = 2131099652;
+			
+			// aapt resource value: 0x7f060002
+			public const int invalid_date = 2131099650;
+			
+			// aapt resource value: 0x7f060000
+			public const int library_name = 2131099648;
+			
+			// aapt resource value: 0x7f060003
+			public const int month_name_format = 2131099651;
 			
 			static String()
 			{
@@ -118,6 +272,31 @@ namespace TriCare.Droid
 			}
 			
 			private String()
+			{
+			}
+		}
+		
+		public partial class Style
+		{
+			
+			// aapt resource value: 0x7f070001
+			public const int CalendarCell = 2131165185;
+			
+			// aapt resource value: 0x7f070003
+			public const int CalendarCell_CalendarDate = 2131165187;
+			
+			// aapt resource value: 0x7f070002
+			public const int CalendarCell_DayHeader = 2131165186;
+			
+			// aapt resource value: 0x7f070000
+			public const int CalendarTitle = 2131165184;
+			
+			static Style()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Style()
 			{
 			}
 		}
