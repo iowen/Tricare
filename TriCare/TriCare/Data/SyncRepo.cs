408a7e0f323a5f87359a4a2cff0a130f298a795a
iowen <owatson@1of1inc.com>
2015-01-14 03:24:22 -0500

    missing sync file

--- TriCare/TriCare/Data/SyncRepo.cs
+++ TriCare/TriCare/Data/SyncRepo.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using SQLite.Net;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Forms;
 
-namespace TriCare.Utilities
+namespace TriCare.Data
 {
-    class SyncUtility
+    public class SyncRepo
     {
+         protected SQLiteConnection database;
+
+        public SyncRepo()
+        {
+            database = DependencyService.Get<ISqLite>().GetConnection();
+            database.CreateTable<Patient>();
+        }
+
+        public List<Patient> GetAllPatients()
+        {
+            return (from i in database.Table<Patient>() select i).ToList();
+        }
     }
 }
