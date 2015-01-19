094114f0584f0b6efd20cef78122e1e111da400a
iowen8 <iowen@Owens-iMac.fios-router.home>
2015-01-18 23:13:56 -0500

    login timeout

--- TriCare/TriCare.Android/obj/Debug/android/src/tricare/droid/MainActivity.java
+++ TriCare/TriCare.Android/obj/Debug/android/src/tricare/droid/MainActivity.java
@@ -11,6 +11,10 @@ public class MainActivity
 		__md_methods = 
 			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
 			"n_onCreateOptionsMenu:(Landroid/view/Menu;)Z:GetOnCreateOptionsMenu_Landroid_view_Menu_Handler\n" +
+			"n_onStart:()V:GetOnStartHandler\n" +
+			"n_onResume:()V:GetOnResumeHandler\n" +
+			"n_onPause:()V:GetOnPauseHandler\n" +
+			"n_onStop:()V:GetOnStopHandler\n" +
 			"";
 		mono.android.Runtime.register ("TriCare.Droid.MainActivity, TriCare.Android, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", MainActivity.class, __md_methods);
 	}
@@ -38,6 +42,38 @@ public class MainActivity
 	}
 
 	private native boolean n_onCreateOptionsMenu (android.view.Menu p0);
+
+
+	public void onStart ()
+	{
+		n_onStart ();
+	}
+
+	private native void n_onStart ();
+
+
+	public void onResume ()
+	{
+		n_onResume ();
+	}
+
+	private native void n_onResume ();
+
+
+	public void onPause ()
+	{
+		n_onPause ();
+	}
+
+	private native void n_onPause ();
+
+
+	public void onStop ()
+	{
+		n_onStop ();
+	}
+
+	private native void n_onStop ();
 
 	java.util.ArrayList refList;
 	public void monodroidAddReference (java.lang.Object obj)
